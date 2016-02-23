6bf06f6fb68403107b60c9ce67458bfe0e63e904
May Pongpitpitak <may.pognpitpitak@aggregateiq.com>
2016-02-22 21:55:39 -0800

    Implement spinning
    The channel now spins to send client update on the state of the spinner.
    The spinner genserver is now also spinning updating its score. The time
    is currently arbitrary just to show a working concept. The code also
    needs a little clean up.
    This will do for now.
    Next is documentation and figure out how to write tests for genserver.

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Gyro.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Score"
   end
 end
