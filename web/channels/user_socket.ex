4462c6d6891079d76a432b2ab9a17394ea870b4f
May Pongpitpitak <may.pognpitpitak@aggregateiq.com>
2016-02-22 21:57:19 -0800

    Add connected time to the socket on connect
    This will come in handy later.

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -21,6 +21,7 @@ defmodule Gyro.UserSocket do
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
   def connect(_params, socket) do
+    socket = assign(socket, :connected_at, :calendar.universal_time())
     {:ok, socket}
   end
 
