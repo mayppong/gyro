998aa3d9bd496b52586b3933a59d6edb414c642f
May Pongpitpitak <me@mayppong.com>
2016-05-01 13:13:09 -0700

    Remove start shortcut method for arena
    This GenServer should always be started with the Gyro application on its
    start up anyway. Since it needs to be part of the application tree, the
    start method isn't needed.

--- lib/gyro/arena.ex
+++ lib/gyro/arena.ex
@@ -5,24 +5,13 @@ defmodule Gyro.Arena do
   alias Gyro.Spinner
   alias Gyro.Squad
 
-  defstruct legendary_spinners: [], legendary_squads: [],
+  defstruct spinner_roster: %{}, squad_roster: %{},
+    legendary_spinners: [], legendary_squads: [],
     heroic_spinners: [], heroic_squads: [],
     loudest_squads: []
 
   @name :arena
   @timer 1000
-
-  @doc """
-  Shortcut method for starting
-  """
-  def start do
-    case start_link(%Arena{}) do
-      {:ok, pid} ->
-        # :timer.send_interval(@timer, pid, :spin)
-        {:ok, pid}
-      {:error, reason} -> {:error, reason}
-    end
-  end
 
   @doc """
   Start a GenServer. A permutation without param input is needed for the
