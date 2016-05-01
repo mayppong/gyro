3dd56f558d8cf41db9ba85f583e18e52bd1cda74
May Pongpitpitak <me@mayppong.com>
2016-05-01 13:28:58 -0700

    Add Agents to Arena GenServer process.
    Start 2 Agents when start the Arena GenServer for holding the list of
    the spinners and list of squads active in the system. Because the list
    could be large and doesn't need to be recalculate, it's better to put it
    separately from the GenServer process itself.
    This should keep the state like and make the spinning a little less
    intensive.

--- lib/gyro/arena.ex
+++ lib/gyro/arena.ex
@@ -5,7 +5,7 @@ defmodule Gyro.Arena do
   alias Gyro.Spinner
   alias Gyro.Squad
 
-  defstruct spinner_roster: %{}, squad_roster: %{},
+  defstruct spinner_roster: nil, squad_roster: nil,
     legendary_spinners: [], legendary_squads: [],
     heroic_spinners: [], heroic_squads: [],
     loudest_squads: []
@@ -14,12 +14,19 @@ defmodule Gyro.Arena do
   @timer 1000
 
   @doc """
-  Start a GenServer. A permutation without param input is needed for the
-  Application Supervisor children.
+  Start the arena GenServer by first starts 2 Agents: one for tracking active
+  spinners, and another for tracking active squads in the system. One both
+  Agents are started successfully, we then start the Arena GenServer.
+  If either of the Agents fails to start, it returns the error from that
+  Agent and not starts the arena GenServer.
   """
-  def start_link, do: start_link(%Arena{})
-  def start_link(state) do
-    GenServer.start_link(__MODULE__, state, name: {:global, @name})
+  def start_link(state \\ %Arena{}) do
+    with {:ok, spinner_roster} <- Agent.start_link((fn() -> [] end)),
+    {:ok, squad_roster} <- Agent.start_link((fn() -> [] end))
+    do
+      state = %{state | spinner_roster: spinner_roster, squad_roster: squad_roster}
+      GenServer.start_link(__MODULE__, state, name: {:global, @name})
+    end
   end
 
 end
