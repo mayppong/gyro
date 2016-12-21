218a6f8e17b612713843418963abf13dabda6609
May Pongpitpitak <me@mayppong.com>
2016-12-20 20:10:42 -0800

    Add new unit test cases for arena message handler

--- test/gyro/arena_test.exs
+++ test/gyro/arena_test.exs
@@ -13,6 +13,33 @@ defmodule Gyro.ArenaTest do
     {:ok, socket: socket, spinner_pid: socket.assigns[:spinner_pid]}
   end
 
+  test "handle enlist" do
+    {:ok, spinner_pid} = Spinner.start_link()
+    {:noreply, state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
+    assert state.size == 1
+  end
+
+  test "handle delist" do
+    {:ok, spinner_pid} = Spinner.start_link()
+    {:noreply, enlisted_state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
+
+    {:noreply, delisted_state} = Arena.handle_cast({:delist, spinner_pid}, enlisted_state)
+    assert delisted_state.size == 0
+  end
+
+  test "handle info" do
+    {:reply, state, _} = Arena.handle_call(:introspect, nil, %Arena{})
+    assert state == %Arena{}
+  end
+
+  test "handle process down" do
+    {:ok, spinner_pid} = Spinner.start_link()
+    {:noreply, enlisted_state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
+
+    {:noreply, delisted_state} = Arena.handle_info({:DOWN, nil, :process, spinner_pid, nil}, enlisted_state)
+    assert delisted_state.size == 0
+  end
+
   test "adding a new spinner" do
     {:ok, spinner_pid} = Spinner.start_link()
 
