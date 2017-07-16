42a363a9e090a1b3e6cfa56b7d39ecf83ace1ac9
May Pongpitpitak <me@mayppong.com>
2017-07-16 12:56:27 -0700

    Group Arena test together using `describe`

--- test/gyro/arena_test.exs
+++ test/gyro/arena_test.exs
@@ -13,55 +13,59 @@ defmodule Gyro.ArenaTest do
     {:ok, socket: socket, spinner_pid: socket.assigns[:spinner_pid]}
   end
 
-  test "handle enlist" do
-    {:ok, spinner_pid} = Spinner.start_link()
-    {:noreply, state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
-    assert state.size == 1
+  describe "GenServer callback implementations" do
+    test "handle enlist" do
+      {:ok, spinner_pid} = Spinner.start_link()
+      {:noreply, state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
+      assert state.size == 1
+    end
+
+    test "handle delist" do
+      {:ok, spinner_pid} = Spinner.start_link()
+      {:noreply, enlisted_state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
+
+      {:noreply, delisted_state} = Arena.handle_cast({:delist, spinner_pid}, enlisted_state)
+      assert delisted_state.size == 0
+    end
+
+    test "handle info" do
+      {:reply, state, _} = Arena.handle_call(:introspect, nil, %Arena{})
+      assert state == %Arena{}
+    end
+
+    test "handle process down" do
+      {:ok, spinner_pid} = Spinner.start_link()
+      {:noreply, enlisted_state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
+
+      {:noreply, delisted_state} = Arena.handle_info({:DOWN, nil, :process, spinner_pid, nil}, enlisted_state)
+      assert delisted_state.size == 0
+    end
   end
 
-  test "handle delist" do
-    {:ok, spinner_pid} = Spinner.start_link()
-    {:noreply, enlisted_state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
+  describe "public API" do
+    test "adding a new spinner" do
+      {:ok, spinner_pid} = Spinner.start_link()
 
-    {:noreply, delisted_state} = Arena.handle_cast({:delist, spinner_pid}, enlisted_state)
-    assert delisted_state.size == 0
-  end
+      Arena.enlist(:spinners, spinner_pid)
+      %{members: members} = Arena.introspect(:spinners)
+      listed_pid = Map.get(members, spinner_pid)
 
-  test "handle info" do
-    {:reply, state, _} = Arena.handle_call(:introspect, nil, %Arena{})
-    assert state == %Arena{}
-  end
+      assert spinner_pid == listed_pid
+    end
 
-  test "handle process down" do
-    {:ok, spinner_pid} = Spinner.start_link()
-    {:noreply, enlisted_state} = Arena.handle_cast({:enlist, spinner_pid}, %Arena{})
+    test "removing a spinner", %{spinner_pid: spinner_pid} do
+      Arena.delist(:spinners, spinner_pid)
+      %{members: members} = Arena.introspect(:spinners)
+      listed_pid = Map.get(members, spinner_pid)
 
-    {:noreply, delisted_state} = Arena.handle_info({:DOWN, nil, :process, spinner_pid, nil}, enlisted_state)
-    assert delisted_state.size == 0
-  end
+      assert listed_pid == nil
+    end
 
-  test "adding a new spinner" do
-    {:ok, spinner_pid} = Spinner.start_link()
+    test "inspecting the arena" do
+      state = Arena.introspect(:spinners)
 
-    Arena.enlist(:spinners, spinner_pid)
-    %{members: members} = Arena.introspect(:spinners)
-    listed_pid = Map.get(members, spinner_pid)
-
-    assert spinner_pid == listed_pid
-  end
-
-  test "removing a spinner", %{spinner_pid: spinner_pid} do
-    Arena.delist(:spinners, spinner_pid)
-    %{members: members} = Arena.introspect(:spinners)
-    listed_pid = Map.get(members, spinner_pid)
-
-    assert listed_pid == nil
-  end
-
-  test "inspecting the arena" do
-    state = Arena.introspect(:spinners)
-
-    assert Map.has_key?(state, :members)
+      assert Map.has_key?(state, :members)
+    end
   end
 
 end
