5849be87b23dd039cf9861964cfba9d4b0e783da
May Pongpitpitak <me@mayppong.com>
2017-11-04 00:15:23 -0700

    Add more services to the project's docker-compose
    Added a few new services like testing containers, and app building
    service.

--- config/test.exs
+++ config/test.exs
@@ -15,5 +15,5 @@ config :gyro, Gyro.Repo,
   username: "postgres",
   password: "postgres",
   database: "gyro_test",
-  hostname: "localhost",
+  hostname: "postgres",
   pool: Ecto.Adapters.SQL.Sandbox
