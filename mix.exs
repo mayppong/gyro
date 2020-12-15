cc1f8fedca53831e064832239a2b2c8d0a88eff2
May Pongpitpitak <me@mayppong.com>
2020-12-14 21:16:45 -0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Gyro.Mixfile do
 
   def project do
     [app: :gyro,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.9",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
